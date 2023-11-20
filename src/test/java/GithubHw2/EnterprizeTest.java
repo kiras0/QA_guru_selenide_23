ed2957b271834d733dc76495cad8065cdbdd05f4
Kiras0 <mihail97.97@gmail.com>
2023-11-20 19:35:40 +0000

    Hover test for selenide#2 HW

---
+++ src/test/java/GithubHw2/EnterprizeTest.java
@@ -0,0 +1,23 @@
+package GithubHw2;
+
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class EnterprizeTest {
+
+
+    @Test
+    void hoverTest(){
+        open("https://github.com");
+        $("ul.d-lg-flex.list-style-none").$(byText("Solutions")).hover();
+        $("[href='/enterprise']").click();
+        $("#hero-section-brand-heading").shouldHave(Condition.exactText("The AI-powered developer platform."));
+    }
+
+}

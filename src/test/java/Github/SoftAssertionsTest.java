30e9b4d01e96d3945f18006ca33d8f476d7585c0
Kiras0 <mihail97.97@gmail.com>
2023-11-20 17:06:02 +0000

    SoftAssertion Test for JUnit5

---
+++ src/test/java/Github/SoftAssertionsTest.java
@@ -0,0 +1,41 @@
+package Github;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SoftAssertionsTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.baseUrl = "https://github.com";
+    }
+
+    @Test
+    void selenideSoftTest() {
+    open("/selenide/selenide");
+    $("#wiki-tab").click();
+    $("#wiki-pages-filter").setValue("Soft");
+    $(".wiki-rightbar").shouldHave(text("SoftAssertions"));
+    $(".wiki-rightbar").$(byText("SoftAssertions")).click();
+        $("#user-content-3-using-junit5-extend-test-class")
+                .closest("h4").sibling(0).shouldHave(text("    @ExtendWith({SoftAssertsExtension.class})" +
+                                                          "    class Tests {" +
+                                                          "      @Test" +
+                                                          "      void test() {" +
+                                                          "        Configuration.assertionMode = SOFT;" +
+                                                          "        open(\"page.html\");" +
+                                                          " " +
+                                                          "        $(\"#first\").should(visible).click();" +
+                                                          "        $(\"#second\").should(visible).click();" +
+                                                          "      }" +
+                                                          "    }"));
+
+   }
+
+
+}
